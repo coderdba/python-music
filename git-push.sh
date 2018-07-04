15a91605910276f060c3a7a792ca7520ef4724f5
coderdba <coderdba@yahoo.com>
2018-07-04 13:45:40 +0530

    updates

--- git-push.sh
+++ git-push.sh
@@ -1,2 +1,8 @@
-git remote add origin git@github.com:coderdba/python-music.git
-git push -u origin master
+#git remote add origin git@github.com:coderdba/python-music.git
+#git push -u origin master
+# Uncomment these if git commit cribs saying email and name are needed
+# git config user.email "you@example.com"
+# git config user.name "Your Name"
+git add .
+git commit -m "updates"
+git push origin master
